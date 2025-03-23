b8f7077b5e7b11292ad5b7e718e29df48b42aedc
sajodajo <samjones116@gmail.com>
2025-03-23 17:21:51 +0100

    new

--- Home.py
+++ Home.py
@@ -7,12 +7,13 @@ from pySimFinLIB import pySimFin
 import plotly.graph_objects as go
 
 
-
 st.set_page_config(
     page_title="Stock Price Analysis",
     page_icon='https://cdn.cookielaw.org/logos/e8ca50c1-d340-4b7f-b6e8-46a90c64bb3d/018fba4d-83ca-7f69-91e4-19e5d2ce04fb/b675201b-d73d-430b-8f8b-058904a17cc5/logo_IE_circle.png',
     layout = 'wide'
 )
+
+
 
 
 
